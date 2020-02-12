0cd8c2b532da7369f6cec35ba2b41cec015b8eb7
Ashwin <ashwinlaly@gmail.com>
2020-02-12 21:57:44 +0530

    login change

--- src/container/Login.js
+++ src/container/Login.js
@@ -2,27 +2,35 @@ import React, { useState } from "react";
 import { Button, FormGroup, FormControl, label, ButtonToolbar, Navbar, Nav, Form } from "react-bootstrap";
 import "./Login.css";
 
+
+
 export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [userName, setUserName ] = useState('');
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
   }
 
   function handleSubmit(event) {
+
+    console.log( email, password, userName);
+    
+
      event.preventDefault();
   }
 
   return (
     <>
     <Navbar bg="primary" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Navbar.Brand href="#home">Home</Navbar.Brand>
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
+      {/* <Nav.Link href="#home">Home</Nav.Link>
       <Nav.Link href="#features">Features</Nav.Link>
       <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
+    */}
+    </Nav> 
     <Form inline>
       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
       <Button variant="outline-light">Search</Button>
@@ -31,6 +39,16 @@ export default function Login(props) {
 
     <div className="Login">
       <form onSubmit={handleSubmit}>
+      <label className={'label-cls'} >Username</label>
+        <FormGroup controlId="username" bsSize="large">
+          <FormControl
+            autoFocus
+            type="username"
+            value={userName}
+            onChange={e => setUserName(e.target.value)}
+            placeholder={'user name'}
+          />
+        </FormGroup>
       <label className={'label-cls'} >Email</label>
         <FormGroup controlId="email" bsSize="large">
           <FormControl
@@ -38,6 +56,7 @@ export default function Login(props) {
             type="email"
             value={email}
             onChange={e => setEmail(e.target.value)}
+            placeholder={'email'}
           />
         </FormGroup>
         <label className={'label-cls'} >Password</label>
@@ -46,6 +65,7 @@ export default function Login(props) {
             value={password}
             onChange={e => setPassword(e.target.value)}
             type="password"
+            placeholder={'password'}
           />
         </FormGroup>
         <ButtonToolbar>
